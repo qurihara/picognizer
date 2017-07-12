dbd1ae849329a083d156234e993abe17602a2836
uemaik <silverlining2e.s@gmail.com>
2017-07-12 13:40:29 +0900

    check

--- index5.js
+++ index5.js
@@ -75,7 +75,8 @@ window.onload = function () {
 		mode:mod,//"direct",  //comparison
 		feature:[ft],//["mfcc"],
 		framesec:frame,//0.1,
-		duration:dur//1.0
+		duration:dur,//1.0
+		slice:[0,1.5]
 	};
 	console.log("picognizer option: " + JSON.stringify(option));
 	P.init(option); //パラメータ設定 (初期化)
